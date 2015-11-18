52363603866aa72b19d743f08712d2b2abc4d3af
umroh <umrohmachfudza@gmail.com>
2015-11-18 17:35:23 +0000

    ch2 done : First FT specced out in the comments, and now use unittest

--- functional_tests.py
+++ functional_tests.py
@@ -1,30 +1,42 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
+class NewVisitorTest(unittest.TestCase):
+	
+	def setUp(self):
+		self.browser = webdriver.Firefox()
+		self.browser.implicitly_wait(3)
 
-# Edith has heard about a cool new online to-do app. She goes
-# to check out its homepage
-browser.get('http://localhost:8000')
+	def tearDown(self):
+		self.browser.quit()
+	
+	def test_can_start_a_list_and_retrieve_it_later(self):
+		# Edith has heard about a cool new online to-do app. She goes
+		# to check out its homepage
+		self.browser.get('http://localhost:8000')
 
-# She notices the page title and header mention to-do lists
-assert 'To-Do' in browser.title
+		# She notices the page title and header mention to-do lists
+		self.assertIn('To-Do', self.browser.title)
+		self.fail('Finish the test!')
 
-# She is invited to enter a to-do item straight away
+		# She is invited to enter a to-do item straight away
 
-# She types "Buy peacock feathers" into a text box (Edith's hobby
-# is tying fly-fishing lures
+		# She types "Buy peacock feathers" into a text box (Edith's hobby
+		# is tying fly-fishing lures
 
-# When she hits enter, the page updates, and now the page lists
-# "1: Buy peacock feathers" as an item in a to-do list
+		# When she hits enter, the page updates, and now the page lists
+		# "1: Buy peacock feathers" as an item in a to-do list
 
-# There is still a text box inviting her to add another item. She
-# enters "Use peacock feathers to make a fly" (Edith is very methodical)
+		# There is still a text box inviting her to add another item. She
+		# enters "Use peacock feathers to make a fly" (Edith is very methodical)
 
-# The page update again, and now shows both items on her list
+		# The page update again, and now shows both items on her list
 
-# Edith wonders whether the site will remember her list. Then she sees
-# that the site has generated a unique URL for her -- there is some
-# explanatory text to that effect.
+		# Edith wonders whether the site will remember her list. Then she sees
+		# that the site has generated a unique URL for her -- there is some
+		# explanatory text to that effect.
 
-# Satisfied, she goes back to sleep
-browser.quit()
+		# Satisfied, she goes back to sleep
+
+if __name__ == '__main__':
+	unittest.main(warnings='ignore')
